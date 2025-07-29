bd24318baff802f0e00fa556ecc550fb93b8706f
grudzinskio <grudzinskio@msoe.edu>
2025-07-29 16:12:03 -0500

    moved where you put env file

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+
+require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = 3001;
